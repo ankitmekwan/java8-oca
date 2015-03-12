4ac4d52754812b76a4b873cbd28e9c32d38fd872
Marcos Savoury <marcossavoury@hotmail.com>
2015-03-12 18:27:47 -0400

    more notes

--- README.md
+++ README.md
@@ -12,3 +12,21 @@ It is not possible to construct a date or time object directly
 ```
 LocalDate d1 = new LocalDate(); // WILL NOT COMPILE
 ```
+
+You cannot have a static method and a non-static method with the same name in the same class
+```
+//WILL NOT COMPILE
+public class StaticTest {
+    public static void test() {
+        //do nothing
+    }
+
+    public void test() {
+        //do nothing
+    }
+
+    public static void main(String [] args) {
+        test();
+    }
+}
+```
--- Switching.java
+++ Switching.java
@@ -17,12 +17,56 @@ public class Switching {
             case 'b': System.out.println("b");
                       break;
         }
+        System.out.println("");
 
-        //This will print nothing since there is no match and no default case
+        switch(a) {
+            case 'b': System.out.println("b");
+            //case 'b': System.out.println("b"); // WILL NOT COMPILE, duplicate case
+                      break;
+        }
+        System.out.println("");
+
+        //This will print default
+        switch(a) {
+            case 'b': System.out.println("b");
+                      break;
+            default:  System.out.println("default case");
+        }
+        System.out.println("");
+
+        //This will print a and b because of the fallthrough
         switch(a) {
             case 'a': System.out.println("a");
             case 'b': System.out.println("b");
                       break;
         }
+        System.out.println("");
+
+        //This will only print 'a'
+        switch(a) {
+            case 'a': System.out.println("a");
+                      break;
+            case 'b': System.out.println("b");
+                      break;
+        }
+        System.out.println("");
+
+        //Prints default, d and m
+        switch(a) {
+            case 'b': System.out.println("b");
+            case 'e': System.out.println("e");
+            case 'c': System.out.println("c");
+            default:  System.out.println("default case");
+            case 'd': System.out.println("d");
+            case 'm': System.out.println("m");
+        }
+        System.out.println("");
+
+        /* Will not compile, can't switch on doubles
+        double db = 10.0;
+        switch (db) {
+            case 11: System.out.println("won't work");
+        }
+        */
     }
 }

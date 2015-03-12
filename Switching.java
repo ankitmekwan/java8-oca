2a32f05c516b495270365d568bb5fd3b0c9b7f57
Marcos Savoury <marcossavoury@hotmail.com>
2015-03-11 23:14:10 -0400

    Adding examples for switch statements

---
+++ Switching.java
@@ -0,0 +1,21 @@
+/*
+ * Examples of switch case statements
+ */
+public class Switching {
+    public static void main(String [] args) {
+        switch1();
+
+    }
+
+    public static void switch1() {
+        char a = 'a';
+
+        switch(a) { } //EMPTY SWITCH WILL COMPILE
+
+        //This will print nothing since there is no match and no default case
+        switch(a) {
+            case 'b': System.out.println("b");
+                      break;
+        }
+    }
+}

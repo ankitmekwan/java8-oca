0d7fc0cda057cd69ec05e4dce8ff6a16df1f25d3
Marcos Savoury <marcossavoury@hotmail.com>
2015-03-14 14:59:20 -0400

    Imports.java

---
+++ Imports.java
@@ -0,0 +1,10 @@
+import java.sql.*;
+import java.util.*;
+
+public class Imports {
+
+    public static void main(String[] args) {
+        //Date d = new Date(); //WILL NOT COMPILE WITH THIS STATEMENT, Date is ambiguous
+    }
+
+}

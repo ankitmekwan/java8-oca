9f5fe874b929e83ffa99ef6b4355e47b375db380
Marcos Savoury <marcossavoury@hotmail.com>
2015-03-15 12:28:03 -0400

    More examples

---
+++ ObjectReferenceToArray.java
@@ -0,0 +1,10 @@
+/*
+ * Example showing that you can assign an array to an object reference
+ */
+ class W {
+    public static void main(String [] args) {
+        int [] values = new int[10];
+
+        Object k = values;
+    }
+}

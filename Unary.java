dce342e8ef2227f78bd917cece2544539a8785e1
Marcos Savoury <marcossavoury@hotmail.com>
2015-03-25 09:44:07 -0400

    more examples

--- Unary.java
+++ Unary.java
@@ -3,5 +3,13 @@ public class Unary {
         int i = 0;
         i = i++; //Tricky, i is assigned itself before the increment runs so it never changes
         System.out.println("I is " + i); //prints 0
+
+        int k = 0;
+        k = ++k;
+        System.out.println("K is " + k); //prints 1
+
+        int j = 0;
+        j++;
+        System.out.println("J is " + j);
     }
 }
